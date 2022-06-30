5c290f9388873c7920bf0a0b1c71c0582caaac4b
Ethan <EthanHrytsak@gmail.com>
2022-06-29 21:40:56 -0600

    added the nvim

---
+++ nvim/init.lua
@@ -0,0 +1,48 @@
+local keybinds = require('keybind')
+local plugins = require('plugins')
+vim.cmd([[ :cd %:p:h ]])
+	
+
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.tsserver.setup{}
+
+
+vim.opt.background = "dark" -- or "light" faor light mode
+vim.cmd([[colorscheme gruvbox
+]])
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+
+command -nargs=1 T :Telescope <args>
+]])
+
+vim.opt.termguicolors = true
+
+vim.opt.smd = false --turn off mode mesage
+vim.g.noru = true -- turno off ruler
+
+local o = vim.o
+
+
+
+
+o.nu = true
+o.rnu = true
+o.smartcase = true
+o.scrolloff = 8
+o.incsearch = true
+o.hlsearch = false
+
+
+
+
+
+
+
+
+
+
+
---
+++ nvim/lua/keybind.lua
@@ -0,0 +1,26 @@
+function map( mode, shortcut, command)
+	vim.api.nvim_set_keymap(mode,shortcut, command,{noremap = true, silent = true})
+end
+
+
+vim.g.mapleader = ' '
+
+-- Variabels
+--
+
+
+
+map('i','jk','<Esc>')
+map('n','<leader>ff',"<cmd>lua require('telescope.builtin').find_files()<cr>")
+map('n','<leader>fc',"<cmd>lua require('telescope.bui:cmltin').commands()<cr>")
+--map('n','<C-leader>',"<cmd>lua require('telescope.builtin').quickfix()<cr>")
+map('n','<leader>fq',"<cmd>lua require('telescope.builtin').pickers()<cr>")
+
+
+-- Using Lua functions
+map('n','<leader>ff','<cmd>lua  require("telescope.builtin").find_files()<cr>')
+map('n','<leader>fg','<cmd>lua  require("telescope.builtin").live_grep()<cr>')
+map('n','<leader>fb','<cmd>lua  require("telescope.builtin").buffers()<cr>')
+map('n','<leader>fh','<cmd>lua  require("telescope.builtin").help_tags()<cr>')
+
+
