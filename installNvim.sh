cb779e7548141d17cf4501c5d358baca6c8af4fe
Ethan Hrytsak <EthanHrytsk@gmail.com>
2023-04-08 16:24:44 -0600

    added simple bash script for installing Nvim config

---
+++ installNvim.sh
@@ -0,0 +1,6 @@
+echo "copying files to ~/config/"
+cp nvim ~/.config/ -r
+echo "cloning packer to directory"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+echo "launch nvim and run :PackerInstall"
